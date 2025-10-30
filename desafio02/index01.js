d41c4077486210b6103fbac48dbe00c916cebfd8
Matheus Augusto Daminelli <matheus.daminelli@gmail.com>
2025-10-30 19:19:11 -0300

    adiciona resolução prova

--- desafio02/index01.js
+++ desafio02/index01.js
@@ -1,1 +1,39 @@
-console.log("Hello, World! 001");
+class LegacyPaymentSystem {
+  makePayment(amount) {
+    console.log(`Pagando R$${amount} com sistema legado.`);
+  }
+}
+
+class ModernPaymentAPI {
+  process(amountInCents) {
+    console.log(`Pagamento de R$${amountInCents / 100} via API moderna.`);
+  }
+}
+
+class PaymentProcessor {
+  constructor(system) {
+    this.system = system;
+  }
+
+  pay(amount) {
+    this.system.makePayment(amount);
+  }
+}
+
+// Adapter 
+class ModernPaymentAdapter {
+  constructor(api) {
+    this.api = api;
+  }
+
+  makePayment(amount) {
+    const amountInCents = Math.round(amount * 100);
+    this.api.process(amountInCents);
+  }
+}
+
+// Cliente
+const modernApi = new ModernPaymentAPI();
+const modernAdapter = new ModernPaymentAdapter(modernApi);
+const processor = new PaymentProcessor(modernAdapter);
+processor.pay(100);
